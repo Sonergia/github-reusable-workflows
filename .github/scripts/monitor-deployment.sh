c2e3a73ec841e35f393756eaeed2fa7992f5b9af
jledoux-sonergia <julien.ledoux@sonergia.fr>
2022-10-06 17:27:08 +0200

    DEVOPS-160 Refactoring workflows in a main one

---
+++ .github/scripts/monitor-deployment.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# Notes:
+# A successful deployment is completed after about 5min
+# A failed deployment is roll out by this script after about 10 min
+# A failed deployment is roll out automatically by deployment_circuit_breaker after at least 30min (10 tasks must fail)
+
+# See https://docs.amazonaws.cn/en_us/AmazonECS/latest/developerguide/deployment-type-ecs.html
+# See https://aws.amazon.com/blogs/containers/announcing-amazon-ecs-deployment-circuit-breaker/
+
+# For testing
+# CLUSTER=test
+# SERVICE=orpheo
+
+echo "------------------- MONITOR DEPLOYMENT -------------------------"
+
+CLUSTER=${1:?CLUSTER is required}
+SERVICE=${2:?SERVICE is required}
+
+SERVICE_DEPLOYMENT_ROLLBACK=0
+SERVICE_DEPLOYMENT_ERROR=0
+MAX_FAILED_TASKS_COUNT=1
+DESCRIBE_SERVICE=$(aws ecs describe-services --cluster ${CLUSTER} --services ${SERVICE})
+SERVICE_DEPLOYMENT_IN_PROGRESS=$(echo "${DESCRIBE_SERVICE}" | jq ".services[].deployments[] \
+    | select(.status == \"PRIMARY\") | select(.rolloutState == \"IN_PROGRESS\")")
+
+while [ ! -z "${SERVICE_DEPLOYMENT_IN_PROGRESS}" ]; do
+    # echo "------------------- LOOP -------------------------"
+
+    DESCRIBE_SERVICE=$(aws ecs describe-services --cluster ${CLUSTER} --services ${SERVICE})
+    SERVICE_DEPLOYMENT_IN_PROGRESS=$(echo "${DESCRIBE_SERVICE}" | jq ".services[].deployments[] \
+        | select(.status == \"PRIMARY\") | select(.rolloutState == \"IN_PROGRESS\")")
+    FAILED_TASKS_COUNT=$(echo "${SERVICE_DEPLOYMENT_IN_PROGRESS}" | jq ".failedTasks")
+    ROLLOUT_STATE_REASON=$(echo "${SERVICE_DEPLOYMENT_IN_PROGRESS}" | jq --raw-output ".rolloutStateReason")
+
+    if [ -z "${INITIAL_ROLLOUT_STATE_REASON}" ]; then
+        INITIAL_ROLLOUT_STATE_REASON=${ROLLOUT_STATE_REASON}
+        echo "::notice title=Monitor deployment:: ${INITIAL_ROLLOUT_STATE_REASON}"
+    fi
+
+    # echo "::debug title=Monitor deployment::FAILED_TASKS_COUNT=${FAILED_TASKS_COUNT}"
+
+    if [ ! -z ${FAILED_TASKS_COUNT} ] && [ ${FAILED_TASKS_COUNT} -gt 0 ]; then
+        echo "::error title=Monitor deployment::Houston we have a problem! ${FAILED_TASKS_COUNT} task(s) failed to start! Max fails is set to ${MAX_FAILED_TASKS_COUNT}!"
+
+        # When reaching max fails => roll back to previous working version
+        if [ ${FAILED_TASKS_COUNT} -ge ${MAX_FAILED_TASKS_COUNT} ]; then
+            # Get a previous working task definition (which is inactive now)
+            # And create a new revision from the previous working one
+            SERVICE_DEPLOYMENT_ROLLBACK=1
+            WORKING_SERVICE_TASK_ARN=$(echo "${DESCRIBE_SERVICE}" | jq --raw-output ".services[].deployments[] \
+                | select(.status == \"ACTIVE\") | select(.rolloutState == \"COMPLETED\") | .taskDefinition")
+
+            if [ ! -z "${WORKING_SERVICE_TASK_ARN}" ]; then
+                echo "::warning title=Monitor deployment::Roll back service with a previous working task definition"
+
+                PREVIOUS_WORKING_TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition ${WORKING_SERVICE_TASK_ARN})
+                NEW_TASK_DEFINITION=$(echo "${PREVIOUS_WORKING_TASK_DEFINITION}" | jq ".taskDefinition \
+                    | del(.taskDefinitionArn) \
+                    | del(.revision) \
+                    | del(.status) \
+                    | del(.requiresAttributes) \
+                    | del(.compatibilities) \
+                    | del(.registeredAt) \
+                    | del(.registeredBy) \
+                    | del(.deregisteredAt)")
+                REGISTER_NEW_TASK_DEFINITION=$(aws ecs register-task-definition --cli-input-json "${NEW_TASK_DEFINITION}")
+                NEW_TASK_DEFINITION_ARN=$(echo "${REGISTER_NEW_TASK_DEFINITION}" | jq --raw-output ".taskDefinition.taskDefinitionArn")
+                SERVICE_ROLLBACK=$(aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE} \
+                    --task-definition ${NEW_TASK_DEFINITION_ARN})
+            else
+                SERVICE_DEPLOYMENT_ERROR=1
+                echo "::error title=Monitor deployment::Could not find a previous working task definition to roll back onto! This is bad, better call your DevOps!"
+
+                break
+            fi
+        fi
+    elif [[ $(echo "${ROLLOUT_STATE_REASON}" | grep "rolling back") ]]; then
+        SERVICE_DEPLOYMENT_ROLLBACK=1
+        echo "::warning title=Monitor deployment:: ${ROLLOUT_STATE_REASON}"
+    elif [ "${INITIAL_ROLLOUT_STATE_REASON}" != "${ROLLOUT_STATE_REASON}" ]; then
+        INITIAL_ROLLOUT_STATE_REASON=""
+    fi
+
+    sleep 10
+done
+
+SERVICE_DEPLOYMENT_COMPLETED=$(echo "${DESCRIBE_SERVICE}" | jq ".services[].deployments[] \
+    | select(.status == \"PRIMARY\") | select(.rolloutState == \"COMPLETED\")")
+
+if [ ! -z "${SERVICE_DEPLOYMENT_COMPLETED}" ]; then
+    echo "------------------- DEPLOYMENT COMPLETED -------------------------"
+
+    if [ ${SERVICE_DEPLOYMENT_ROLLBACK} == 1 ]; then
+        echo "::warning title=Monitor deployment::Service was roll backed!"
+    else
+        echo "::notice title=Monitor deployment::Deployment completed"
+    fi
+else
+    # detecter le rollback :
+    #  * status=PRIMARY rolloutState=IN_PROGRESS rolloutStateReason="*rolling back to*"
+    #  * status=ACTIVE rolloutState=FAILED rolloutStateReason="*task failed to start*"
+    # extra: chercher dans les events la cause si rolloutStateReason insuffisant: "failed container health checks"
+
+    SERVICE_DEPLOYMENT_FAILED=$(echo "${DESCRIBE_SERVICE}" | jq ".services[].deployments[] \
+        | select(.status == \"ACTIVE\") | select(.rolloutState == \"FAILED\")")
+
+    if [ ! -z "${SERVICE_DEPLOYMENT_FAILED}" ]; then
+        echo "------------------- DEPLOYMENT FAILED -------------------------"
+
+        ROLLOUT_STATE_REASON=$(echo "${SERVICE_DEPLOYMENT_FAILED}" | jq --raw-output ".rolloutStateReason")
+        echo "::error title=Monitor deployment::Deployment failed, rolloutStateReason: ${ROLLOUT_STATE_REASON}"
+    fi
+fi
+
+# Status must be one of:
+# error, failure, inactive, in_progress, queued, pending, success
+# success: 2cbe4e, failure: ffc107, error: cb2431
+if [ ${SERVICE_DEPLOYMENT_ERROR} == 1 ]; then
+    echo "::set-output name=DEPLOY_STATUS::error"
+elif [ ${SERVICE_DEPLOYMENT_ROLLBACK} == 1 ]; then
+    echo "::set-output name=DEPLOY_STATUS::failure"
+else
+    echo "::set-output name=DEPLOY_STATUS::success"
+fi
--- .github/scripts/set-context.sh
+++ .github/scripts/set-context.sh
@@ -9,13 +9,11 @@
 # echo "GITHUB_REF_NAME   => $GITHUB_REF_NAME"
 # echo "GITHUB_REF_TYPE   => $GITHUB_REF_TYPE"
 # echo "GITHUB_SHA        => $GITHUB_SHA"
-# echo "IMAGE_STATIC_TAG  => $IMAGE_STATIC_TAG"
 # exit 1
 
 # # Successful case workflow_dispatch
 # GITHUB_REF_TYPE=branch
 # GITHUB_EVENT_NAME=workflow_dispatch
-# IMAGE_STATIC_TAG=latest
 # CLUSTER=release
 
 # # Successful case latest
@@ -24,7 +22,6 @@
 # GITHUB_REF=refs/pull/1234/merge
 # GITHUB_HEAD_REF=feature/TOTO-4567_blablabla
 # GITHUB_REF_NAME=feature/TOTO-4567_blablabla
-# IMAGE_STATIC_TAG=latest
 
 # # Successful case branch
 # GITHUB_REF_TYPE=branch
@@ -47,11 +44,38 @@
 # GITHUB_HEAD_REF=
 # GITHUB_REF_NAME=1.0.0
 
-CREATE_TAG_LATEST="false"
+# Extract JIRA code from branch name
+function getJiraCodeFromBranch {
+    # Get PR number from GITHUB_REF
+    # PR_NUMBER=$(echo ${GITHUB_REF} | awk 'BEGIN { FS = "/" } ; { print $3 }')
 
-# Manual case uses image static tag (such as "latest")
+    # Check if source branch is project
+    if [[ ${GITHUB_REF_NAME} =~ ^(project|epic)/.*$ ]]; then
+        # Extract JIRA codes from branch pattern (ex: project/MAESTRO-1186/feature/MAESTRO-1151)
+        JIRA_CODE=$(echo ${GITHUB_REF_NAME} | awk 'BEGIN { FS = "/" } ; { print $4 }')
+    else
+        # Extract JIRA codes from branch pattern (ex: feature/MAESTRO-1280, or tech/DEVOPS-160_refactoring)
+        JIRA_CODE=$(echo ${GITHUB_REF_NAME} | awk 'BEGIN { FS = "/" } ; { print $2 }')
+    fi
+
+    # Check JIRA ticket code format and clean up if not compliant
+    JIRA_CODE=$(echo ${JIRA_CODE} | grep -P -o '^[A-Z]{2,}-[0-9]+')
+
+    if [ -z ${JIRA_CODE} ]; then
+        echo "::error title=Set context::Naming convention error: could not extract JIRA ticket code from source branch ref '${GITHUB_REF_NAME}'"
+        # echo "::error title=Set context::Naming convention debug: GITHUB_HEAD_REF='${GITHUB_HEAD_REF}' GITHUB_REF='${GITHUB_REF}' GITHUB_REF_NAME='${GITHUB_REF_NAME}'"
+        exit 1
+    fi
+}
+
+# Start script
+CREATE_TAG_LATEST="false"
+COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s")
+
+# Manual case uses image static tag
 if  [ ${GITHUB_EVENT_NAME} == "workflow_dispatch" ]; then
-    IMAGE_TAG=${IMAGE_STATIC_TAG}
+    getJiraCodeFromBranch
+    IMAGE_TAG=${JIRA_CODE}
 elif  [ ${GITHUB_REF_TYPE} == "tag" ] && [ ${GITHUB_EVENT_NAME} == "release" ]; then
     # Tag git
     IMAGE_TAG=${GITHUB_REF_NAME}
@@ -64,48 +88,30 @@ elif  [ ${GITHUB_REF_TYPE} == "tag" ] && [ ${GITHUB_EVENT_NAME} == "release" ]; 
         CLUSTER=prod
         CREATE_TAG_LATEST="true"
     else
-        echo "::error title=Define vars::Tag format error: your git tag does not respect Semantic Versioning, see https://regex101.com/r/vkijKf/1/"
-        echo "::debug::Could not define cluster with ref_type '${GITHUB_REF_TYPE}' and ref_name '${GITHUB_REF_NAME}'"
+        echo "::error title=Set context::Tag format error: your git tag does not respect Semantic Versioning, see https://regex101.com/r/vkijKf/1/"
+        echo "::debug title=Set context::Could not set cluster with ref_type '${GITHUB_REF_TYPE}' and ref_name '${GITHUB_REF_NAME}'"
         exit 1
     fi
 elif [ ${GITHUB_REF_TYPE} == "branch" ] && [ ${GITHUB_EVENT_NAME} == "pull_request" ]; then
-    # Get PR number from GITHUB_REF
-    # PR_NUMBER=$(echo ${GITHUB_REF} | awk 'BEGIN { FS = "/" } ; { print $3 }')
-
-    # Check if source branch is project
-    if [[ ${GITHUB_HEAD_REF} =~ ^project/.*$ ]]; then
-        # Extract JIRA codes from branch pattern (ex: project/MAESTRO-1186/feature/MAESTRO-1151)
-        # JIRA_PROJECT_CODE=$(echo ${GITHUB_HEAD_REF} | awk 'BEGIN { FS = "/" } ; { print $2 }')
-        JIRA_TICKET_CODE=$(echo ${GITHUB_HEAD_REF} | awk 'BEGIN { FS = "/" } ; { print $4 }')
-    else
-        # Extract JIRA codes from branch pattern (ex: feature/MAESTRO-1280)
-        JIRA_TICKET_CODE=$(echo ${GITHUB_HEAD_REF} | awk 'BEGIN { FS = "/" } ; { print $2 }')
-    fi
-
-    # Check JIRA ticket code format and clean up if not compliant
-    JIRA_TICKET_CODE=$(echo ${JIRA_TICKET_CODE} | grep -P -o '^[A-Z]{2,}-[0-9]+')
-
-    if [ -z ${JIRA_TICKET_CODE} ]; then
-        echo "::error title=Define vars::Naming convention error: could not extract JIRA ticket code from PR source branch ref '${GITHUB_HEAD_REF}'"
-        exit 1
-    fi
-
-    IMAGE_TAG=${JIRA_TICKET_CODE}
+    getJiraCodeFromBranch
+    IMAGE_TAG=${JIRA_CODE}
     CLUSTER=test
 else
-    echo "::error title=Define vars::Not implemented case with event '${GITHUB_EVENT_NAME}' and ref_type '${GITHUB_REF_TYPE}'"
+    echo "::error title=Set context::Not implemented case with event '${GITHUB_EVENT_NAME}' and ref_type '${GITHUB_REF_TYPE}'"
     exit 1
 fi
 
-echo "::notice title=Define vars::Cluster output value is '${CLUSTER}'"
-echo "::notice title=Define vars::Image tag output value is '${IMAGE_TAG}'"
-echo "::notice title=Define vars::Create tag latest is '${CREATE_TAG_LATEST}'"
+echo "::notice title=Set context::Cluster output value is '${CLUSTER}'"
+echo "::notice title=Set context::Image tag output value is '${IMAGE_TAG}'"
+echo "::notice title=Set context::Create tag latest is '${CREATE_TAG_LATEST}'"
 
 if [ -z ${CLUSTER} ] || [ -z ${IMAGE_TAG} ]; then
-    echo "::error title=Define vars::Could not define one or more required vars with event '${GITHUB_EVENT_NAME}', ref_type '${GITHUB_REF_TYPE}', ref_name '${GITHUB_REF_NAME}'"
+    echo "::error title=Set context::Could not set one or more required vars with event '${GITHUB_EVENT_NAME}', ref_type '${GITHUB_REF_TYPE}', ref_name '${GITHUB_REF_NAME}'"
     exit 1
 fi
 
 echo "::set-output name=CLUSTER::${CLUSTER}"
+echo "::set-output name=JIRA_CODE::${JIRA_CODE}"
 echo "::set-output name=IMAGE_TAG::${IMAGE_TAG}"
 echo "::set-output name=CREATE_TAG_LATEST::${CREATE_TAG_LATEST}"
+echo "::set-output name=COMMIT_MESSAGE::${COMMIT_MESSAGE}"
